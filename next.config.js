4cc218a23f0c13411c22f325fd42e001e32eeb3e
bg-vc <vincecfl@gmail.com>
2024-11-29 15:40:38 +0800

    feat: update project configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jsdelivr.net',
         port: '',
         pathname: '/**',
       },
